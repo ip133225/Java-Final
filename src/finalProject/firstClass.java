1e2fc426cc54973577617ca9e83a2d495e66cee1
MHancock5466 <mhancock5466@gmail.com>
2017-03-03 14:41:40 -0600

    Update

---
+++ src/finalProject/firstClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package finalProject;
+
+/**
+ * @author MH137428
+ *
+ */
+public class firstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
