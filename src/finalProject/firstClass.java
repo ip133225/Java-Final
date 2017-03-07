615603f82f6a16cb3049c5ade37ce1e41f4dbf24
MHancock5466 <mhancock5466@gmail.com>
2017-03-07 14:56:30 -0600

    Update

--- src/finalProject/firstClass.java
+++ src/finalProject/firstClass.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 public class firstClass 
 {
 
+	private static String replay;
 	private static int weapon, food, vehicle, survivors, home;
 	
 	public static void main(String[] args) 
@@ -66,8 +67,23 @@ public class firstClass
 		JOptionPane.showMessageDialog(null, "Congratulations! You survived the four days required for help to arrive. You win!");
 		if(vehicle == 0 && food == 0 && weapon == 0)
 			JOptionPane.showMessageDialog(null, "You survived with no weapon, vehicle, or extra food. Well done.");
-		else if(vehicle == 1)
-			JOptionPane.showMessageDialog(null, )
+	}
+	
+	public static void foodLossMessage()
+	{
+		JOptionPane.showMessageDialog(null, "You died because you couldn't find any food. Better luck next time.");
+		replay = JOptionPane.showInputDialog(null, "Would you like to play again? Type Yes or No");
+	}
+	
+	public static void zombieHorde()
+	{
+		JOptionPane.showInputDialog(null, "Oh no! A zombie horde has found you wandering around. Do you choose to fight or flee from the horde?"
+				+ "(Hint: If you have no weapon, do not fight)");
+	}
+	
+	public static String homeBase()
+	{
+		JOptionPane.showInputDialog(null, "");
 	}
 	
 }
