3aac6dca23d1bab65e4191172fd13275508acf8e
Isaiah Ponciano <zai.cruz@rocketmail.com>
2017-03-08 13:47:23 -0600

    Changes

--- src/finalProject/Choose.java
+++ src/finalProject/Choose.java
@@ -12,10 +12,13 @@ import javax.swing.JOptionPane;
 public class Choose 
 {
 
-	public static void zombieHorde()
+	public static void chooseFightorFlee()
 	{
 		JOptionPane.showInputDialog(null, "Oh no! A zombie horde has found you wandering around. Do you choose to fight or flee from the horde?"
 				+ "(Hint: If you have no weapon, do not fight)");
 	}
-	
+	public static void chooseVehicle()
+	{
+		JOptionPane.showInputDialog(null, "");
+	}
 }
