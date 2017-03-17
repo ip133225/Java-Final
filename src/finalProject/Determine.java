40c889ea6b8db4a3e5bc06e6b6844538e28a4233
MHancock5466 <mhancock5466@gmail.com>
2017-03-17 10:49:09 -0500

    Update

--- src/finalProject/Determine.java
+++ src/finalProject/Determine.java
@@ -10,7 +10,7 @@ package finalProject;
 public class Determine 
 {
 
-	public static boolean weapon(int weapon)
+	public static boolean weapon(int weapon)		//Determines if user has a weapon
 	{
 		boolean hasWeapon = false;
 		if(weapon > 0)
@@ -18,7 +18,7 @@ public class Determine
 		return hasWeapon;
 	}
 
-	public static boolean food(int food)
+	public static boolean food(int food)			//Determines if user has food
 	{
 		boolean hasFood = true;
 		if(food < 1)
@@ -26,7 +26,7 @@ public class Determine
 		return hasFood;
 	}
 
-	public static boolean vehicle(int vehicle)
+	public static boolean vehicle(int vehicle)		//Determines if user has a vehicle
 	{
 		boolean hasVehicle = false;
 		if(vehicle > 0)
@@ -34,7 +34,7 @@ public class Determine
 		return hasVehicle;
 	}
 
-	public static boolean survivors(int survivors)
+	public static boolean survivors(int survivors)	//Determines if user has survivors
 	{
 		boolean hasSurvivors = false;
 		if(survivors > 0)
@@ -42,7 +42,7 @@ public class Determine
 		return hasSurvivors;
 	}
 
-	public static boolean inCity(int location)
+	public static boolean inCity(int location)		//Determines user's location
 	{
 		boolean inCity = false;
 		if(location == 0)
